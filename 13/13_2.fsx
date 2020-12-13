f7f432e21d145287bfa14d71273a5466e1994cf5
Charles Rea <charles.rea@ghyston.com>
2020-12-13 11:53:34 +0000

    Refactor 13.2 to be more readable

--- 13/13_2.fsx
+++ 13/13_2.fsx
@@ -5,12 +5,12 @@ open Common
 
 let input = File.ReadAllLines "./input.txt"
 
-type Incidence = {
+type Occurrences = {
     StartPoint: int64
     Frequency: int64
 }
 
-let buses: Incidence list =
+let buses: Occurrences list =
     input.[1]
     |> splitString ","
     |> Seq.mapi (fun i x -> x, i)
@@ -21,15 +21,20 @@ let buses: Incidence list =
 let rec gcd (x: int64) (y: int64) = if y = 0L then abs x else gcd y (x % y)
 let lcm (x: int64) (y: int64) = x * y / (gcd x y)
 
-let findIncidences (a: Incidence) (b: Incidence): Incidence =
-    let firstIntersection =
-            Seq.initInfinite (fun i -> (int64 i) * a.Frequency + a.StartPoint)
-            |> Seq.filter (fun occurrenceOfA -> (occurrenceOfA - b.StartPoint) % b.Frequency = 0L)
-            |> Seq.head
-    { StartPoint = firstIntersection; Frequency = lcm a.Frequency b.Frequency }
+let allValues (occurrences: Occurrences): int64 seq =
+    Seq.initInfinite (fun i -> (int64 i) * occurrences.Frequency + occurrences.StartPoint)
 
-let incidenceOfAllBuses =
-    List.fold findIncidences (List.head buses) (List.tail buses)
+let containsValue (occurrences: Occurrences) (value: int64): bool =
+    (value - occurrences.StartPoint) % occurrences.Frequency = 0L
 
-printf "%d" incidenceOfAllBuses.StartPoint
+let findIntersections (a: Occurrences) (b: Occurrences): Occurrences =
+    {
+        StartPoint = allValues a |> Seq.filter (b |> containsValue) |> Seq.head
+        Frequency = lcm a.Frequency b.Frequency
+    }
+
+let occurrencesOfAllBuses =
+    List.fold findIntersections (List.head buses) (List.tail buses)
+
+printf "%d" occurrencesOfAllBuses.StartPoint
 
